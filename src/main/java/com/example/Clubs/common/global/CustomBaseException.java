72d8709fe975c99591a1c34b0745fd15a076e2fb
Doeun Lee <lde28700409@gmail.com>
2025-09-06 12:07:03 +0900

    CustomBaseException 수정 및 mysql 추가
    Signed-off-by: doeun0913 <lde28700409@gmail.com>

--- src/main/java/com/example/Clubs/common/global/CustomBaseException.java
+++ src/main/java/com/example/Clubs/common/global/CustomBaseException.java
@@ -7,5 +7,12 @@ public abstract class CustomBaseException extends RuntimeException {
     public CustomBaseException(CommonException exception) {
         super(exception.getMessage());
     }
+
+    public CustomBaseException(String message) {}
+
+    public CustomBaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
     public abstract CommonException getErrorCode();
 }
